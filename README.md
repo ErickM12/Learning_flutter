59d2a146aef8a86997c3358bc15f92443cdc206a
ErickMirelesMerchant <erick.mireles@therocketcode.com>
2024-11-11 15:00:00 -0600

    primer commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Learning_flutter
+# learning_flutter_b
+
+A new Flutter project.
---
+++ lib/modules/map/map_sample.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapSample extends StatefulWidget {
+  const MapSample({super.key});
+
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(18.850639346596104, -99.20114335862581),
+    zoom: 14.4746,
+    bearing: 192.8334901395799,
+    tilt: 59.440717697143555,
+  );
+//18.850639346596104, -99.20114335862581
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+        width: double.infinity,
+        height: 350,
+          child: GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
